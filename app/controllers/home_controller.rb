96933fe4574cfb0ab6d14f0deaa03c373d7d2ede
Mark Blair <mark@onthecity.org>
2013-05-01 11:16:44 -0700

    initial commit for citytest app

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,50 @@
+require 'openssl'
+
+class HomeController < ApplicationController
+
+  def index
+    @city_data = decrypt_city_data(params[:city_data], params[:city_data_iv])
+    if @city_data.present?
+      @city_auth_data = authentication_data(@city_data["oauth_token"])
+    end
+  end
+
+  private
+
+  def decrypt_city_data(city_data, city_data_iv)
+    if city_data.present?
+      string_to_decrypt = Base64.urlsafe_decode64(city_data)
+      iv = Base64.urlsafe_decode64(city_data_iv)
+      json_data = city_decrypt(string_to_decrypt, THE_CITY_APP_SECRET.first(32), iv)
+      return ActiveSupport::JSON.decode(json_data)
+    else
+      return {}
+    end
+  end
+
+  def city_decrypt(encrypted_data, key, iv, cipher_type = "AES-256-CBC")
+    aes = OpenSSL::Cipher::Cipher.new(cipher_type)
+    aes.decrypt
+    aes.key = key
+    aes.iv = iv if iv != nil
+    aes.update(encrypted_data) + aes.final
+  end
+
+  def authentication_data(auth_token)
+    full_url = "http://authentication.devthecity.org:3002/authorization"
+    response = Typhoeus::Request.send(:get, full_url, :params => {:access_token => auth_token})
+    response_body = {}
+    if response.success?
+      response_body = (JSON.parse(response.body) rescue {})
+    elsif response.timed_out?
+      puts "got a time out"
+    elsif response.code == 0
+      puts response.curl_error_message
+    else
+      puts "HTTP request failed: " + response.code.to_s
+    end
+    return response_body
+  end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytestapp::Application.config.secret_token = 'd668bbbec5ed49cb20f101188e49e117e8053cd0769f2d6db523bc93be8262b4424af4fea180af5d747f1c0062251fac4d94209d2ef8b451f1c42bef63591bef'
